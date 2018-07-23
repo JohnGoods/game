dfeaad77732edb0c043fc7eba46136afbe1b361e
JiangJianghao <469672930@qq.com>
2018-07-23 17:19:10 +0800

    程序表单

---
+++ 程序表单/client_copy.bat
@@ -0,0 +1,9 @@
+﻿::设置表单路径
+set TARGET_PATH=D:\h5\program\client\trunk\2dgame\resource\language\zh-cn\data\config
+
+xcopy %~dp0common\*.* %TARGET_PATH% /E /y /i
+xcopy %~dp0client\*.* %TARGET_PATH% /E /y /i
+::xcopy %~dp0server\Battle\SkillInfo.csv %TARGET_PATH%\skill /E /y /i
+::xcopy %~dp0server\Monster\Monster.csv %TARGET_PATH%\skill /E /y /i
+
+pause
---
+++ 程序表单/server_copy.bat
@@ -0,0 +1,9 @@
+﻿::设置表单路径
+set TARGET_PATH=Z:\server\data\csv
+
+xcopy %~dp0common\*.* %TARGET_PATH% /E /y /i
+xcopy %~dp0server\*.* %TARGET_PATH% /E /y /i
+::xcopy %~dp0server\Battle\SkillInfo.csv %TARGET_PATH%\skill /E /y /i
+::xcopy %~dp0server\Monster\Monster.csv %TARGET_PATH%\skill /E /y /i
+
+pause
